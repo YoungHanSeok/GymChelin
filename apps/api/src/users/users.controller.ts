fbf18e37e176056539b9ecb4be206298fc3c7df0
석영한 <sywsyw159@maius.co.kr>
2025-12-01 13:11:36 +0900

    초기 화면 구성 #4

--- apps/api/src/users/users.controller.ts
+++ apps/api/src/users/users.controller.ts
@@ -1,13 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Redirect } from '@nestjs/common';
 import { UsersService } from './users.service';
 
-@Controller('api/users') // 라우팅 경로를 '/users'로 설정
+@Controller('api') // 라우팅 경로를 '/users'로 설정
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
   
-  @Get() // GET 요청 핸들러
+  @Get('users') // GET 요청 핸들러
   findAll() {
     console.log('UsersController: findAll called');
     return this.usersService.findAll();
+  }
+
+  @Post('test')
+  testRedirect(@Body() params: object) {
+    console.log("type params : " + typeof(params));
+    console.log('params : ' + params.toString());
+    return params;
   }
 }
