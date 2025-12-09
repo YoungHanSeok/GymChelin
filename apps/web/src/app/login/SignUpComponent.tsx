2aebc21beec886cca981b6c90a708dbc78ee1f08
석영한 <sywsyw159@maius.co.kr>
2025-12-09 17:12:50 +0900

    로그인 개발 중#2

--- apps/web/src/app/login/SignUpComponent.tsx
+++ apps/web/src/app/login/SignUpComponent.tsx
@@ -2,27 +2,40 @@
 
 import api from "@/lib/api";
 import Modal from "../_components/modalComponent";
-import { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
+import {useEffect, useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 
 const SignUpModal = ({ onClose }: { onClose: () => void }) => {
-    useEffect(() => {
-        api.get("/users")
-        .then((response) => {
-            console.log("Users:", response.data);
-        }).catch((error) => {
-            console.error("Error fetching users:", error);
-        });
-    },[])
+    interface SignUpFormData {
+      email: string;
+      id: string;
+      password: string;
+      passwordCheck: string;
+    }
 
-    const { register,handleSubmit } = useForm({
+    const { register, handleSubmit, formState: { errors,isSubmitting,isValid },watch,getValues,trigger }= useForm<SignUpFormData>({
+      mode : "onChange",
       defaultValues : {
-        email: '',
-        id: '',
-        password: '',
-        passwordCheck: ''
+        email : "",
+        id : "",
+        password : "",
+        passwordCheck : ""
       }
     });
+    
+    const watchEmail = watch("email");
+
+    useEffect(() => {
+      console.log("watchEmail : "+watchEmail);
+      console.log("register : "+getValues("email"));
+      //console.log("trigger : "+trigger("email"));    
+        
+    },[watchEmail])
+
+
+
+   
+
 
     /**
      * 이메일 중복 체크 함수
@@ -37,8 +50,9 @@ const SignUpModal = ({ onClose }: { onClose: () => void }) => {
 
     };
 
-    const submitForm = () => {
-      console.log("register : "+register.toString());
+    const submitForm : SubmitHandler<SignUpFormData> = (data) => {
+      console.log("aaa");
+      console.log("data : "+data);
     }
 
 
@@ -47,12 +61,20 @@ const SignUpModal = ({ onClose }: { onClose: () => void }) => {
       <h2 className="mb-6 text-center text-2xl font-semibold">회원가입</h2>
 
       <form className="space-y-4" onSubmit={handleSubmit(submitForm)}>
-        <div>
+        <div className="h-20">
           <label className="block text-sm font-medium text-gray-700">
             이메일
           </label>
           <div className="flex items-center space-x-2">
-            <input type="text" className="input-style flex-grow" {...register("email",{required : true})}/>
+            <input type="text" required className="input-style flex-grow" {
+              ...register("email",{
+                    required : true,
+                    pattern : {
+                      value : /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                      message : "유효한 이메일 형식이 아닙니다."
+                    }
+                  })
+              }/>
             <button
               type="button"
               className="whitespace-nowrap rounded-md bg-gray-600 px-4 py-2 text-sm font-medium text-white hover:bg-gray-700"
@@ -60,9 +82,11 @@ const SignUpModal = ({ onClose }: { onClose: () => void }) => {
             >
               중복 확인
             </button>
+            
           </div>
+          {errors.email && <p className="text-red-500 text-sm">{errors.email.message}</p>}
         </div>
-        <div>
+        <div className="h-20">
           <label className="block text-sm font-medium text-gray-700">
             아이디
           </label>
@@ -77,13 +101,13 @@ const SignUpModal = ({ onClose }: { onClose: () => void }) => {
             </button>
           </div>
         </div>
-        <div>
+        <div className="h-20">
           <label className="block text-sm font-medium text-gray-700">
             비밀번호
           </label>
           <input type="password" required className="input-style" {...register("password",{required : true})}/>
         </div>
-        <div>
+        <div className="h-20">
           <label className="block text-sm font-medium text-gray-700">
             비밀번호 확인
           </label>
